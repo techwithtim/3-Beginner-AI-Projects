9efe41895a2da3e7c5677faab25ccbe284b86b5c
Ryan Kroetch <77401166+LaSquadra@users.noreply.github.com>
2025-07-18 22:18:02 -0400

    Update main.py
    Correcting small typo in project1

--- project1/main.py
+++ project1/main.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 @tool
 def calculator(a: float, b: float) -> str:
-    """Useful for performing basic arithmeric calculations with numbers"""
+    """Useful for performing basic arithmetic calculations with numbers"""
     print("Tool has been called.")
     return f"The sum of {a} and {b} is {a + b}"
     
